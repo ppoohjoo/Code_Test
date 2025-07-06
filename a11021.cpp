fc2b516f897e27acfd991547b0ea40ce95908774
ppoohjoo <sogsoaaly@gmail.com>
2025-07-06 18:47:46 +0900

    a11021

---
+++ a11021.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+void a11021() {
+
+
+
+    int x;
+    cin >> x;
+
+    int a, b;
+
+    for (int i = 0; i < x; i++) {
+        cin >> a >> b;
+        cout <<"case #"<< x<< ": " << a - b << '\n';
+    }
+
+
+
+
+
+
+}
