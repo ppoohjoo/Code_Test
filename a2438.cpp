837c321ef714525e4d3f5696e98e2f4479a66e8f
ppoohjoo <sogsoaaly@gmail.com>
2025-07-06 19:11:45 +0900

    a2438

---
+++ a2438.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+void a2438() {
+
+
+
+    int x;
+    cin >> x;
+
+
+    for (int i = 1; i <= x; i++) {
+        for (int k = 0; k < i; k++) {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+
+
+
+
+
+
+}
