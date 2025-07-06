85ede1deefb46fb692bd54095589958c26e366b8
ppoohjoo <sogsoaaly@gmail.com>
2025-07-06 19:45:52 +0900

    a10952

---
+++ a10952.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+void a10952() {
+
+
+
+    int a,b;
+    cin >> a >> b;
+    cout << a + b << "\n";
+
+    while (0 < a&&0 < b) {
+        cin >> a >> b;
+        if (a == 0 && b == 0) {
+            break;
+        }
+
+        cout<< a+b << "\n";
+
+    }
+
+
+
+
+
+
+}
