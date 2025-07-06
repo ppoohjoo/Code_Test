09ac5dc4a3aa3386ea7236620ba5743baf4d5196
ppoohjoo <sogsoaaly@gmail.com>
2025-07-06 18:40:18 +0900

    a15552

---
+++ a15552.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+void a15552() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int x;
+    cin >> x;
+
+    int a, b;
+
+    for (int i = 0; i < x; i++) {
+        cin >> a >> b;
+        cout << a + b << '\n';
+    }
+
+    
+
+
+
+
+}
