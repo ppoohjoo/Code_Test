46c99b06ded091c4a0d7ba4396f19f15d4a1f151
ppoohjoo <sogsoaaly@gmail.com>
2025-07-06 19:01:38 +0900

    a11022

---
+++ a11022.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+void a11022() {
+
+
+
+    int x,a,b;
+    cin >> x;
+
+
+    for (int i = 1; i <= x; i++) {
+        cin >> a >> b;
+        cout << "Case #" << i << ": " << a << " + " << b << " = " << a + b << '\n';
+    }
+
+
+
+
+
+
+}
